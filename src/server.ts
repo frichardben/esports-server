444e607f9177b7db8adb74439a6a98ba7000cf5c
Richard Ben <frichardben@yahoo.com.br>
2022-09-18 09:52:52 -0300

    chore: added cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import { PrismaClient } from '@prisma/client';
 import { convertHoursStringToMinutes } from './utils/convert-hour-string-to-minutes';
 import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string';
@@ -7,6 +9,7 @@ import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-stri
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 const prisma = new PrismaClient();
 

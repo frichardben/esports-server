942bdb73c69ac000c4eb5479ad36144d18927578
Richard Ben <frichardben@yahoo.com.br>
2022-09-18 08:50:52 -0300

    feat: list games

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const app = express;
+const app = express();
+const prisma = new PrismaClient();
+
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    })
+
+    return response.json(games);
+})
+
+app.listen(3333);
